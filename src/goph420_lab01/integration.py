051e015be98b0ed1bc0883978f16491f3fa5dfce
Joshua Oyedemi <josh.oyed@gmail.com>
2025-01-28 10:41:21 -0700

    gauss added

---
+++ src/goph420_lab01/integration.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def  integrate_gauss():
+
+    '''
+    parameters:
+    ==========
+    f: function to integrate
+    lims: limits of integration
+    npts: number of points to use in the integration
+
+    returns:
+    =======
+        float: the integral of f over the interval lims
+    '''
+
+
