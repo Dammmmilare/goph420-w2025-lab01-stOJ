a9522178ea51d003686e132abc321b8ee00a965d
Colin.Maloney <colin.maloney@ucalgary.ca>
2025-02-04 10:16:37 -0700

    another graph

--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -1,7 +1,7 @@
 from __init__ import * 
 
 
-def integrate_newton(x, f, alg): 
+def integrate_newton(x, f, alg="trap"): 
     """
     Integrate a function using Newton-Cotes quadrature. 
     
@@ -18,7 +18,13 @@ def integrate_newton(x, f, alg):
     -------
     float
         The integral of the function. 
-    """ 
+    """  
+
+    if alg.strip().lower() == "trap": 
+        return trapezoid(x, f) 
+    elif alg.strip().lower() == "simp": 
+        return simpson(x, f)
+
     def midpoint(x, f): 
         """uses midpoint rule to integrate a function""" 
         dx = x[1] - x[0]
@@ -29,7 +35,16 @@ def integrate_newton(x, f, alg):
         integral_trap = 0.5 * dx[0] * (f[0] + f[-1])  # First and last points 
         integral_trap += np.sum(dx[0] * f[1:-1])  # Sum of the middle points 
         
-        return integral_trap
+        return integral_trap 
+    
+    def simpson(x, f, dx): 
+        """uses Simpson's rule to integrate a function""" 
+        integral_simp = dx[0] / 3 * (f[0] + f[-1])  # First and last points 
+        integral_simp += np.sum(4 * dx[0] * f[1:-1:2])  # Odd points 
+        integral_simp += np.sum(2 * dx[0] * f[2:-1:2]) # Even points  
+
+        return integral_simp
+
 
     if alg == 'midpoint': 
         return midpoint(x, f)
@@ -53,5 +68,7 @@ def  integrate_gauss():
     returns:
     =======
         float: the integral of f over the interval lims
-    '''
+    ''' 
+
+
 
