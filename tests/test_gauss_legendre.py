5f93b82f32918e3ea33bc3f8661782fb755190ac
Joshua Oyedemi <josh.oyed@gmail.com>
2025-02-11 18:54:12 -0700

    tests lagrange done

--- tests/test_gauss_legendre.py
+++ tests/test_gauss_legendre.py
@@ -1,7 +1,13 @@
-import numpy as np 
+import numpy as np
+import unittest
 from goph420_lab01.integration import ( 
     integrate_gauss, 
 ) 
 
-def test_gauss_legendre(): 
-  pass
+class TestGaussLegendre(unittest.TestCase): 
+    def test_polynomial_exact(self): 
+        """ Test the Gauss-Legendre integration of a polynomial up to order 2N - 1"""
+        f = lambda x: x**3 - x**2 + x - 1
+        result = integrate_gauss(f, -1, 1, 2)
+        expected = -4/3
+        self.assertAlmostEqual(result, expected, places=10)
