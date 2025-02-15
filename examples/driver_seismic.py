7c85b2dd01f2bdda8fbdad9ef9b5ff6da9e80f33
Dammmmilare <josh.oyed@gmail.com>
2025-02-14 17:46:06 -0700

    driver files added

--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -1,9 +1,10 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/goph420_lab01')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import numpy as np
 import unittest
+import matplotlib.pyplot as plt
 from goph420_lab01.integration import (integrate_gauss) 
 
 def main():
