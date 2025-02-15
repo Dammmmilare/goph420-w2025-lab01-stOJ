e2ece9a4ff71b8ed813353cebb326a2833a3f2a6
Dammmmilare <josh.oyed@gmail.com>
2025-02-14 19:03:37 -0700

    code added

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -1,14 +1,15 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
 import numpy as np
-import unittest
 import matplotlib.pyplot as plt
+from data import s_wave_data
 from goph420_lab01.integration import (integrate_gauss) 
 
-def main():
-    unittest.main()
+def probability_density(x):
+    return s_wave_data(x, loc=1.5, scale=0.5)
 
-if __name__ == "__main__":
-    main()
+prob = integrate_gauss(probability_density, [4, np.inf], npts=3)
+print(f"Probability that S-wave velocity is greater than 4: {prob}")
+
+plt.savefig("figures/probability_density_plot.png")
--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -3,12 +3,18 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import numpy as np
-import unittest
 import matplotlib.pyplot as plt
-from goph420_lab01.integration import (integrate_gauss) 
+from goph420_lab01.integration import (integrate_newton) 
 
-def main():
-    unittest.main()
+data = np.loadtxt("data/s_wave_data.txt")
+x,f = data[:,0], data[:,1]
 
-if __name__ == "__main__":
-    main()
+trap_integral = integrate_newton(x, f,alg="trap")
+simp_integral = integrate_newton(x, f,alg="simp")
+
+plt.plot(x, f, label="S-wave data")
+plt.legend()
+plt.savefig("figures/s_wave_data_plot.png")
+
+print(f"Integral using trapezoidal rule: {trap_integral}")
+print(f"Integral using Simpson's rule: {simp_integral}")
