9286d8d387ad9b23f0fdb6b4b8ae785a2c070648
Colin.Maloney <colin.maloney@ucalgary.ca>
2025-02-16 18:10:13 -0700

    got rid of an unused improt statement

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -3,7 +3,6 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import numpy as np
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 from goph420_lab01.integration import (integrate_gauss) 
 
